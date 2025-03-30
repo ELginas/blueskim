bad556628a44d290a2b836c6283611c2c81d8c2a
ELginas <gintaras.z123@yahoo.com>
2025-03-30 19:11:45 +0300

    create server modal part 1

---
+++ app/(main)/(routes)/server/page.tsx
@@ -0,0 +1,5 @@
+const Page = () => {
+  return <div>Server page</div>;
+};
+
+export default Page;
---
+++ components/modals/initial-modal.tsx
@@ -0,0 +1,104 @@
+"use client";
+
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+const formSchema = z.object({
+  name: z
+    .string()
+    .min(1, {
+      message: "Server name is required.",
+    })
+    .max(50, {
+      message: "Server name is too long",
+    }),
+  imageUrl: z.string().min(1, {
+    message: "Server image is required.",
+  }),
+});
+
+export const InitialModal = () => {
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      imageUrl: "",
+    },
+  });
+
+  const isLoading = form.formState.isSubmitting;
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
+
+  return (
+    <Dialog open>
+      <DialogContent className="overflow-hidden bg-white text-black p-0">
+        <DialogHeader className="pt-8 px-6">
+          <DialogTitle className="text-2xl font-bold text-center">
+            Create a server
+          </DialogTitle>
+          <DialogDescription className="text-center text-zinc-500">
+            It looks like you want to create a server? Give it a name and and
+            icon to shine.
+          </DialogDescription>
+        </DialogHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <div className="space-y-8 px-6">
+              <div className="flex items-center justify-center text-center">
+                Image upload
+              </div>
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70">
+                      Server name
+                    </FormLabel>
+                    <FormControl>
+                      <Input
+                        disabled={isLoading}
+                        className="dark:bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0"
+                        placeholder="Enter server name"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage className="text-red-500" />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <DialogFooter className="px-6 py-4 bg-gray-100">
+              <Button disabled={isLoading} variant="primary">
+                Create
+              </Button>
+            </DialogFooter>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+};
