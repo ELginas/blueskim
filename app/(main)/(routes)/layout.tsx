59be71bac6ae7df7c81f52077478a46a60b82387
ELginas <gintaras.z123@yahoo.com>
2025-03-30 15:59:08 +0300

    clerk auth

---
+++ app/(main)/(routes)/layout.tsx
@@ -0,0 +1,24 @@
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <header className="flex justify-end items-center p-4 gap-4 h-16">
+        <SignedOut>
+          <SignInButton />
+          <SignUpButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </header>
+      {children}
+    </>
+  );
+}
