525a726d4f91f018083aee9a5a9c5a3367fb8294
ELginas <gintaras.z123@yahoo.com>
2025-03-31 17:21:54 +0300

    create server modal

--- components/sidebar/navigation-action.tsx
+++ components/sidebar/navigation-action.tsx
@@ -1,19 +1,26 @@
+"use client";
+
 import {
   Tooltip,
   TooltipContent,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
+import { useModal } from "@/hooks/use-modal-store";
 import { Plus } from "lucide-react";
 
-export const NavigationAction = () => (
-  <Tooltip>
-    <TooltipTrigger className="group">
-      <div className="w-[48px] h-[48px] rounded-[24px] flex flex-col items-center justify-center group-hover:rounded-[12px] group-hover:bg-emerald-500 bg-neutral-700 transition-all">
-        <Plus className="w-6 h-6 text-green-500 group-hover:text-white transition" />
-      </div>
-    </TooltipTrigger>
-    <TooltipContent side="right" align="center" className="font-medium">
-      Create a server
-    </TooltipContent>
-  </Tooltip>
-);
+export const NavigationAction = () => {
+  const { onOpen } = useModal();
+
+  return (
+    <Tooltip>
+      <TooltipTrigger className="group" onClick={() => onOpen("createServer")}>
+        <div className="w-[48px] h-[48px] rounded-[24px] flex flex-col items-center justify-center group-hover:rounded-[12px] group-hover:bg-emerald-500 bg-neutral-700 transition-all">
+          <Plus className="w-6 h-6 text-green-500 group-hover:text-white transition" />
+        </div>
+      </TooltipTrigger>
+      <TooltipContent side="right" align="center" className="font-medium">
+        Create a server
+      </TooltipContent>
+    </Tooltip>
+  );
+};
